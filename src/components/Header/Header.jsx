cbc579728ffd474a5c647f0f4f7877c778361b9d
Laila1222 <korosililla@gmail.com>
2020-04-30 12:59:12 +0200

    styling of header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,14 +4,14 @@ import './Header.scss';
 function Header() {
   return (
     <section className="section-header">
-      <div className="header">
-        <h1 className="header__primary-heading">Hi, I am Lilla</h1>
-        <h2 className="header__secondary-heading">
+      <div className="header u-center-middle">
+        <h1 className="header__primary-heading primary-heading">Hi, I am Lilla</h1>
+        <h2 className="header__secondary-heading secondary-heading u-margin-bottom-small">
           A Copenhagen based front end developer with passion in creating
           dynamic websites providing clean and responsive code with
           user-friendly experience.
         </h2>
-        <a href="#" className="header__button">View my work</a>
+        <a href="#" className="header__button ">View my work</a>
       </div>
     </section>
   );
