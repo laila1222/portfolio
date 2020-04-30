cc29213798c2892bab9e10c04ea272b0505d88d3
Laila1222 <korosililla@gmail.com>
2020-04-30 13:08:22 +0200

    added, styled navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,12 +1,20 @@
-import React from 'react';
-import './Navbar.scss';
+import React from "react";
+import "./Navbar.scss";
 
 function Navbar() {
-    return (
-        <div>
-            Navbar
-        </div>
-    )
+  return (
+    <div className="navbar">
+      <a href="#" className="navbar__link">
+        About
+      </a>
+      <a href="#" className="navbar__link">
+        Projects
+      </a>
+      <a href="#" className="navbar__link">
+        Contact
+      </a>
+    </div>
+  );
 }
 
 export default Navbar;
