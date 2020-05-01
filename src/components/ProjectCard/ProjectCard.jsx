8b16e9b0c91c2f8061aae30f66adbe163e23f57e
Laila1222 <korosililla@gmail.com>
2020-05-01 15:23:17 +0200

    added layer and more style to cards

--- src/components/ProjectCard/ProjectCard.jsx
+++ src/components/ProjectCard/ProjectCard.jsx
@@ -5,17 +5,21 @@ function ProjectCard({ imageSrc }) {
   return (
     <div className="card">
       <div className="card__side card__side--front">
-        <img
-          src="/images/weather.jpg"
-          alt="project image"
-          className="card__img"
-        />
+        <img src={imageSrc} alt="project image" className="card__img" />
       </div>
       <div className="card__side card__side--back">
-          <div className="card__cta">
-              <a href="#" className="card__link--code">Check out code</a>
-              <a href="#" className="card__link--code">Visit site</a>
-          </div>
+        <img src={imageSrc} alt="project image" className="card__img" />
+        {/* Layer over image to change color */}
+        <div className="card__layer"></div>
+
+        <div className="card__cta u-center-middle u-flex">
+          <a href="#" className="card__link card__link--code u-text-center">
+            Check out code
+          </a>
+          <a href="#" className="card__link card__link--site u-text-center">
+            Visit site
+          </a>
+        </div>
       </div>
     </div>
   );
