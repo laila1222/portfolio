6ee758484a943b317ba82a4e4187736542f51832
Laila1222 <korosililla@gmail.com>
2020-05-02 18:11:27 +0200

    added contact

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,13 +1,35 @@
-import React from "react";
+import React, { Component } from 'react'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTools } from "@fortawesome/free-solid-svg-icons";
 import { faLaptopCode } from "@fortawesome/free-solid-svg-icons";
+import { faArrowDown } from "@fortawesome/free-solid-svg-icons";
+
+import PlaceHolder from '../../images/mountain.jpg'
 
 import "./About.scss";
 
-function About() {
-  return (
-    <section className="section-about about">
+
+class About extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            featureOpen: false
+                 
+        }
+    }
+
+    arrowClicked = () => {
+        if (!this.state.featureOpen) {
+            this.setState({featureOpen: true}, () => {
+
+            })
+        }
+    }
+
+    render() {
+        return (
+            <section className="section-about about">
       <div className="about__container">
         <h2 className="about__heading secondary-heading u-text-center u-margin-bottom-big">
           About me
@@ -30,13 +52,21 @@ function About() {
               responsibilities.
             </p>
           </div>
-          <div className="about__features u-flex">
-            <div className="feature">
-              <FontAwesomeIcon icon={faTools} className="feature__icon " />
-              <h3 className="feature__heading tertiary-heading u-text-center">
+          <div className="about__image-container">
+              <img src={PlaceHolder} alt="lilla" className="about__image"/>
+          </div>
+          
+        </div>
+        <div className="about__features">
+            <div className="feature feature--tech">
+            
+              <h3 className="feature__heading tertiary-heading">
+              <FontAwesomeIcon icon={faTools} className="feature__icon"/>
                 Technlogoies I work with
+                <FontAwesomeIcon icon={faArrowDown} className="feature__arrow " />
               </h3>
-              <div className="feature__list">
+              
+              <div className="feature__list feature__list--tech">
                 <p className="feature__item">HTML</p>
                 <p className="feature__item">CSS</p>
                 <p className="feature__item">Sass</p>
@@ -47,13 +77,15 @@ function About() {
                 <p className="feature__item">Terminal</p>
               </div>
             </div>
-            <div className="feature">
+            <div className="feature feature--soft">
+              
+              <h3 className="feature__heading tertiary-heading">
               <FontAwesomeIcon icon={faLaptopCode} className="feature__icon " />
-              <h3 className="feature__heading tertiary-heading u-text-center">
               Softwares I use
-               
+              <FontAwesomeIcon icon={faArrowDown} className="feature__arrow " />
               </h3>
-              <div className="feature__list">
+              
+              <div className="feature__list feature__list--soft">
                   <p className="feature__item">StoryBook</p>
                   <p className="feature__item">Firebase, Firestore</p>
                   <p className="feature__item">Postman</p>
@@ -63,14 +95,15 @@ function About() {
                   <p className="feature__item">Figma</p>
                   <p className="feature__item">Photoshop</p>
                   <p className="feature__item">Visual Studio Code</p>
-              </div>
+              </div>        
               
             </div>
           </div>
-        </div>
       </div>
     </section>
-  );
+        )
+    }
 }
 
-export default About;
+export default About
+
--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,9 +1,26 @@
 import React from "react";
-import './Contact.scss';
+import "./Contact.scss";
 
 function Contact() {
   return (
-    <section className="section-contact contact">
+    <section className="section-contact ">
+      <div className="contact u-center-middle">
+        <h2 className="secondary-heading contact__heading u-text-center u-margin-bottom-medium">
+          Let's get in touch 
+        </h2>
+        <p className="contact__text ">
+          Write me an {" "}
+          <a href="#" className="contact__link">
+            email
+          </a>
+        </p>
+        <p className="contact__text ">
+          Find me on {" "}
+          <a href="#" className="contact__link">
+            LinkedIn
+          </a>
+        </p>
+      </div>
     </section>
   );
 }
