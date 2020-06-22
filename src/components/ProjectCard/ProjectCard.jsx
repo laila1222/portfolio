6410003449143c16b3b8dcc2426b46bb7083922c
Laila1222 <korosililla@gmail.com>
2020-06-22 14:46:09 +0200

    fixed projects

--- src/components/ProjectCard/ProjectCard.jsx
+++ src/components/ProjectCard/ProjectCard.jsx
@@ -8,15 +8,25 @@ function ProjectCard({ imageSrc, codesUrl, url }) {
         <img src={imageSrc} alt="project" className="card__img" />
       </div>
       <div className="card__side card__side--back">
-      {/* <img src={imageSrc} alt="project image" className="card__img" /> */}
+        {/* <img src={imageSrc} alt="project image" className="card__img" /> */}
         {/* Layer over image to change color */}
         <div className="card__layer"></div>
 
         <div className="card__cta u-center-middle u-flex">
-          <a href={codesUrl} className="card__link card__link--code u-text-center" target="_blank" rel="noopener noreferrer"> 
+          <a
+            href={codesUrl}
+            className="card__link card__link--code u-text-center"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             See code
           </a>
-          <a href={url} className="card__link card__link--site u-text-center" target="_blank" rel="noopener noreferrer">
+          <a
+            href={url}
+            className="card__link card__link--site u-text-center"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             Visit site
           </a>
         </div>
