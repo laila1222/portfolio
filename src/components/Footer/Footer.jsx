d87db76d1788cb6f83bf02d5d670e192e6741ed1
Laila1222 <korosililla@gmail.com>
2020-06-22 13:02:02 +0200

    added twitter icon

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faGithub } from "@fortawesome/free-brands-svg-icons";
-import { faLinkedinIn } from "@fortawesome/free-brands-svg-icons";
+import { faGithub, faLinkedinIn, faTwitter } from "@fortawesome/free-brands-svg-icons";
+// import { faLinkedinIn } from "@fortawesome/free-brands-svg-icons";
 
 import "./Footer.scss";
 
@@ -12,12 +12,15 @@ function Footer() {
       <div className="footer__container u-center-middle u-text-center">
         <p className="footer__text">
           Made by Lilla Kőrösi, 2020{" "}
-          <a href="https://github.com/Laila1222" className="footer__link">
+          <a href="https://github.com/Laila1222" className="footer__link" target="_blank" rel="noopener noreferrer">
             <FontAwesomeIcon icon={faGithub} className="footer__icon" />{" "}
           </a>{" "}
-          <a href="https://www.linkedin.com/in/lilla-korosi/" className="footer__link">
+          <a href="https://www.linkedin.com/in/lilla-korosi/" className="footer__link" target="_blank" rel="noopener noreferrer">
             <FontAwesomeIcon icon={faLinkedinIn} className="footer__icon" />
           </a>{" "}
+          <a href="https://twitter.com/lilla_korosi" className="footer__link" target="_blank" rel="noopener noreferrer">
+            <FontAwesomeIcon icon={faTwitter} className="footer__icon" />
+          </a>
         </p>
       </div>
     </footer>
